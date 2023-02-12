7545cc83ad39af9f8b65ab6055d74ad2aa11f44e
Eden Lee <deoksoo.lee@smartfoodnet.com>
2023-02-13 00:18:38 +0900

    - ConditionalOnMyClass를 사용하여 특정클래스가 있을경우에 빈으로 올리기

--- src/main/java/com/example/hellospring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/example/hellospring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,30 +1,16 @@
 package com.example.hellospring.config.autoconfig;
 
+import com.example.hellospring.config.ConditionalMyOnClass;
 import com.example.hellospring.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.ClassUtils;
 
 @MyAutoConfiguration
-@Conditional(TomcatWebServerConfig.TomcatCondition.class)
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory(){
         return new TomcatServletWebServerFactory();
-    }
-
-    static class TomcatCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return ClassUtils.isPresent(
-                    "org.apache.catalina.startup.Tomcat",
-                    context.getClassLoader()
-            );
-        }
     }
 }
