9ec1dbeb597be032e2650b42db7c77615bdb5325
Eden Lee <deoksoo.lee@smartfoodnet.com>
2023-02-12 16:07:21 +0900

    - ImportCandidates를 사용하여 외부 설정

--- src/main/java/com/example/hellospring/config/MyAutoConfigurationImportSelector.java
+++ src/main/java/com/example/hellospring/config/MyAutoConfigurationImportSelector.java
@@ -1,14 +1,26 @@
 package com.example.hellospring.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.lang.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyAutoConfigurationImportSelector implements DeferredImportSelector {
-    @Override
-    public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "com.example.hellospring.config.autoconfig.DispatcherServletConfig",
-                "com.example.hellospring.config.autoconfig.TomcatWebServerConfig"
-        };
+
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigurationImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
     }
+
+    @Override
+    @NonNull
+    public String[] selectImports(@NonNull AnnotationMetadata importingClassMetadata) {
+        List<String> autoConfigs = new ArrayList<>();
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+        return autoConfigs.toArray(new String[0]);
+    };
 }
--- src/main/java/com/example/hellospring/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/com/example/hellospring/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,10 @@
 package com.example.hellospring.config.autoconfig;
 
+import com.example.hellospring.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
     @Bean
     public DispatcherServlet dispatcherServlet() {
