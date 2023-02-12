c887d315bbff0e41d12b4728aedf2c811b2fc408
Eden Lee <deoksoo.lee@smartfoodnet.com>
2023-02-12 16:36:30 +0900

    - JettyWebServerFactory 설정 추가 - 현 커밋에서는 2개의 WebServerFacotry(tomcay, jetty)가 있기에 실행이 안된다

---
+++ src/main/java/com/example/hellospring/config/autoconfig/JettyWebServerConfig.java
@@ -0,0 +1,14 @@
+package com.example.hellospring.config.autoconfig;
+
+import com.example.hellospring.config.MyAutoConfiguration;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+@MyAutoConfiguration
+public class JettyWebServerConfig {
+    @Bean("jettyWebServerFactory")
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new JettyServletWebServerFactory();
+    }
+}
--- src/main/java/com/example/hellospring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/example/hellospring/config/autoconfig/TomcatWebServerConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 
 @MyAutoConfiguration
 public class TomcatWebServerConfig {
-    @Bean
+    @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory(){
         return new TomcatServletWebServerFactory();
     }
